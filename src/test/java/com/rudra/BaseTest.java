976a44a22140b34ea75450bacba882a8eebebc79
Rudra <dwasale@gmail.com>
2022-11-30 21:35:27 +0000

    added feature file for registration commit

--- src/test/java/com/rudra/BaseTest.java
+++ src/test/java/com/rudra/BaseTest.java
@@ -1,17 +1,20 @@
 package com.rudra;
 
 import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest extends Utils{
     DriverManager driverManager = new DriverManager();
 
+    //This method runs before calling any other methods or class
     @BeforeMethod
     public void setUp(){
         driverManager.openBrowser();
     }
 
-//    @AfterMethod
+    //This methods is called after all the other methods are called
+    @AfterMethod
     public void tearDown(ITestResult result) throws Exception{
         if (!result.isSuccess()){
             takeScreenshot(result.getName());
